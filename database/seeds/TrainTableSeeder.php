8e3ec215332c55a42be0054a1804dc6e02a2578d
GCaringi <giannicaringi94@gmail.com>
2022-07-12 20:19:23 +0200

    Add seeder and add fake info on db

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Train; 
+
 
 class TrainTableSeeder extends Seeder
 {
@@ -9,8 +12,21 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Faker $faker){
+         
+        for ($i = 0; $i < 30; $i++){
+            
+            $train = new Train();
+
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->date('Y-m-d');
+            $train->arrival_time = $faker->date('Y-m-d');
+            $train->train_code = $faker->regexify('[A-Z]{5}[0-4]{3}[A-Z]{3}');
+            $train->wagon = $faker->numberBetween(0,100);
+            $train->save();
+        }
     }
 }
+
